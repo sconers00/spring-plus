b6953273beea62be9caca1a4d59da3b15ac58822
sconers00 <jungnam369@naver.com>
2025-05-13 12:59:52 +0900

    2-3

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -5,20 +5,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
-import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 
     @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u ORDER BY t.modifiedAt DESC")
     Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
-
-    @Query("SELECT t FROM Todo t " +
-            "LEFT JOIN t.user " +
-            "WHERE t.id = :todoId")
-    Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
 
     @Query("Select t From Todo t Left Join Fetch t.user u Where t.createdAt>:std and t.modifiedAt<:edd ORDER BY t.modifiedAt DESC")
     Page<Todo> findAllByOrderByModifiedAtDescWithDate(Pageable pageable, LocalDate std, LocalDate edd);
--- src/main/java/org/example/expert/domain/todo/service/TodoService.java
+++ src/main/java/org/example/expert/domain/todo/service/TodoService.java
@@ -1,7 +1,10 @@
 package org.example.expert.domain.todo.service;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import lombok.RequiredArgsConstructor;
 import org.example.expert.client.WeatherClient;
 import org.example.expert.domain.common.dto.AuthUser;
@@ -10,6 +13,7 @@ import org.example.expert.domain.todo.dto.request.TodoSaveRequest;
 import org.example.expert.domain.todo.dto.request.TodoSearchRequest;
 import org.example.expert.domain.todo.dto.response.TodoResponse;
 import org.example.expert.domain.todo.dto.response.TodoSaveResponse;
+import org.example.expert.domain.todo.entity.QTodo;
 import org.example.expert.domain.todo.entity.Todo;
 import org.example.expert.domain.todo.repository.TodoRepository;
 import org.example.expert.domain.user.dto.response.UserResponse;
@@ -21,6 +25,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
 
 @Service
 @RequiredArgsConstructor
@@ -85,20 +91,29 @@ public class TodoService {
         ));
     }
 
+    @PersistenceContext
+    EntityManager em;
+
     public TodoResponse getTodo(long todoId) {
-        Todo todo = todoRepository.findByIdWithUser(todoId)
-                .orElseThrow(() -> new InvalidRequestException("Todo not found"));
 
-        User user = todo.getUser();
+        JPAQueryFactory jqf = new JPAQueryFactory(em);
 
-        return new TodoResponse(
-                todo.getId(),
-                todo.getTitle(),
-                todo.getContents(),
-                todo.getWeather(),
-                new UserResponse(user.getId(), user.getEmail()),
-                todo.getCreatedAt(),
-                todo.getModifiedAt()
-        );
+        QTodo todo = QTodo.todo;
+
+        Optional<Todo> todoOptional= Optional.ofNullable(jqf.selectFrom(todo)
+			.where(todo.id.eq(todoId))
+			.fetchOne());
+
+        Todo todoResult = todoOptional.orElseThrow(() -> new InvalidRequestException("Todo not found"));
+
+		return new TodoResponse(
+			todoResult.getId(),
+			todoResult.getTitle(),
+			todoResult.getContents(),
+			todoResult.getWeather(),
+			new UserResponse(todoResult.getUser().getId(), todoResult.getUser().getEmail()),
+			todoResult.getCreatedAt(),
+			todoResult.getModifiedAt());
+
     }
 }
